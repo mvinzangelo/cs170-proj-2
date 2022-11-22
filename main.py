deb42202bb9a1548fe3b3f5dd4c6fd75724ea3de
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-11-21 20:14:32 -0800

    Add function stub for accuracy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import random
+
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    return random.random()
+
 def main():
-    print("Hello world")
+    print(leave_one_out_cross_validation(0,0,0))
      
 if __name__ == "__main__":
     main()
