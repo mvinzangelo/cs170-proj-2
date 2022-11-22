4e21da50b7700c21dd9500bc3ab6c188607c26f9
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-11-21 23:16:12 -0800

    Refactor code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import random
 
-
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    return random.random()
-
-def main():
-    data_path = "./test_data.txt"
+def feature_search(data_path):
     df = pd.read_table(data_path, delim_whitespace=True, header=None)
     for i in range(1, len(df.columns)):
         print(f'I am on the {i}th level of the search tree')
         for j in range(1, len(df.columns)):
             print(f'--Considering adding the {j} feature')
+
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
+    return random.random()
+
+def main():
+    feature_search("./test_data.txt")
 
 if __name__ == "__main__":
     main()
