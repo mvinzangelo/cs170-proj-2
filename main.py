f98f7636815f5a72484acffafc2cc31b1362624f
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-11-21 23:14:58 -0800

    Add inside loop

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     df = pd.read_table(data_path, delim_whitespace=True, header=None)
     for i in range(1, len(df.columns)):
         print(f'I am on the {i}th level of the search tree')
+        for j in range(1, len(df.columns)):
+            print(f'--Considering adding the {j} feature')
 
 if __name__ == "__main__":
     main()
