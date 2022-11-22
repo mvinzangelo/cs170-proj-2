301c8921dd20c7a1940e759a62e2db3639ff19ca
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-11-21 22:31:19 -0800

    Read file with Pandas

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import pandas as pd
 import random
+
+data_path = "./test_data.txt"
+data = pd.read_table(data_path, header=None)
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
     return random.random()
 
 def main():
-    print(leave_one_out_cross_validation(0,0,0))
-     
+    print(data)
+
 if __name__ == "__main__":
     main()
