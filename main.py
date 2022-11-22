fdb392f81ab72752461a0afc94147f7e55f89c8a
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-11-21 23:13:05 -0800

    Iterate over number of columns

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import random
 
-data_path = "./test_data.txt"
-data = pd.read_table(data_path, header=None)
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
     return random.random()
 
 def main():
-    print(data)
+    data_path = "./test_data.txt"
+    df = pd.read_table(data_path, delim_whitespace=True, header=None)
+    for i in range(1, len(df.columns)):
+        print(f'I am on the {i}th level of the search tree')
 
 if __name__ == "__main__":
     main()
