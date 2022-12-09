02591a57e7343701aa8267ab0c568b50eb83c82a
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-12-08 16:23:36 -0800

    Remove percents and add time

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import copy
+import time
 
 def feature_search(data, algorithm):
     current_set_of_features = []
@@ -20,9 +21,9 @@ def feature_search(data, algorithm):
                 # do cross validation and get accuracy with additional feature added
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, j, algorithm)
                 if algorithm == '1':
-                    print(f'---Using features {current_set_of_features} and adding {j} has an accuracy of {accuracy * 100}%')
+                    print(f'---Using features {current_set_of_features} and adding {j} has an accuracy of {accuracy}')
                 elif algorithm == '2':
-                    print(f'---Removing features {current_set_of_features} and also {j} has an accuracy of {accuracy * 100}%')
+                    print(f'---Removing features {current_set_of_features} and also {j} has an accuracy of {accuracy}')
                 # compare the best accuracy with current accuracy to get max for level
                 if accuracy > best_accuracy_so_far:
                     best_accuracy_so_far = accuracy
@@ -40,64 +41,75 @@ def feature_search(data, algorithm):
             found_max = False
         # print best accuracy at this level
         if algorithm == '1':
-            print(f'Feature set {current_set_of_features} was the best, accuracy is {best_accuracy_so_far * 100}%')
+            print(f'Feature set {current_set_of_features} was the best, accuracy is {best_accuracy_so_far0}')
         elif algorithm == '2':
-            print(f'Removing feature set {current_set_of_features} was the best, accuracy is {best_accuracy_so_far * 100}%')
+            print(f'Removing feature set {current_set_of_features} was the best, accuracy is {best_accuracy_so_far}')
     # print best features and accuracy for the search
     if algorithm == '1':
-        print(f'\nFinished search! The best feature subset is {best_set}, which has an accuracy of {best_accuracy * 100}%')
+        print(f'\nFinished search! The best feature subset is {best_set}, which has an accuracy of {best_accuracy}')
     elif algorithm == '2':
-        print(f'\nFinished search! The best feature subset to remove is {best_set}, which has an accuracy of {best_accuracy * 100}%')
+        print(f'\nFinished search! The best feature subset to remove is {best_set}, which has an accuracy of {best_accuracy}')
     
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add, algorithm):
     number_correctly_classified = 0
+    # create list of all features included in this iteration
     tmp_set = copy.deepcopy(current_set)
     tmp_set.append(feature_to_add)
     columns_to_drop = None
+    # based on algorithm, find the column that will be zeroed out
     if algorithm == '1':
         columns_to_drop = list(range(1, len(data[0])))
         # from https://stackoverflow.com/questions/4211209/remove-all-the-elements-that-occur-in-one-list-from-another
         columns_to_drop = [x for x in columns_to_drop if x not in tmp_set]
     elif algorithm == '2':
         columns_to_drop = tmp_set
+    # loop through all rows
     for i, row_i in enumerate(data):
         label_object_to_classify = row_i[0]
         nearest_neighbor_distance = float('inf')
         nearest_neighbor_location = float('inf') 
         nearest_neighbor_label = float('inf')
+        # loop through all rows
         for j, row_j in enumerate(data):
+            # make sure to remove itself from nearest neighbor classification
             if j == i:
                 continue
+            # compute euclidean distance
             distance = euclidean_distance(row_i, row_j, columns_to_drop)
+            # find shortest distance and keep track of its index and label
             if distance < nearest_neighbor_distance:
                 nearest_neighbor_distance = distance
                 nearest_neighbor_location = j
                 nearest_neighbor_label = row_j[0]
+        # see if nearest neighbor classified the removed row correctly
         if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified += 1
+    # calculate accuracy
     accuracy = number_correctly_classified / (len(data))
-    return accuracy
+    return round(accuracy, 3)
 
 def euclidean_distance(row_a, row_b, zeroed_rows):
     distance = 0
     for i in range(1, len(row_a)):
         if i in zeroed_rows:
             continue
+        # distance formula calculation
         distance += (row_a[i] - row_b[i]) ** 2
     return np.sqrt(distance)
 
 def main():
     file_path_to_test = input("Type in name of file to test: ")
     type_of_algorithm = input("\nWhat type of algorithm do you want to run? (1: Forward Selection 2: Backward Elimination): ")
-
     df = pd.read_table(file_path_to_test, delim_whitespace=True, header=None)
     print(f'\n{file_path_to_test} has {df.shape[1] - 1} features (not including the class attribute), with {df.shape[0]} instances.')
     data_dict = df.to_dict('records')
-    starting_accuracy = leave_one_out_cross_validation(data_dict, [x for x in range(1, len(data_dict[0]) - 1)], len(data_dict[0]), '1')
-    print(f'\nRunning nearest neighbor with all {df.shape[1] - 1} features, using "leave-one-out" evaluation, I get an accuracy of {starting_accuracy * 100}%')
+    starting_accuracy = leave_one_out_cross_validation(data_dict, [], [], '1')
+    print(f'\nRunning nearest neighbor with all {df.shape[1] - 1} features, using "leave-one-out" evaluation, I get an accuracy of {starting_accuracy }')
     print('\nBeginning search.')
+    start_time = time.time()
     feature_search(data_dict, type_of_algorithm)
+    print("Time: %s seconds" % round(time.time() - start_time, 2))
 
 if __name__ == "__main__":
     main()
